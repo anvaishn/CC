d1efda068a09fa77c51d3a268da98b80e7d81428
anvaishn <107908817+anvaishn@users.noreply.github.com>
2022-08-19 17:15:10 +0900

    Added Laplace

--- Hatom.py
+++ Hatom.py
@@ -1,20 +1,26 @@
 import numpy as np
+import numpy.matlib
 #testing meshgrid
-nx, ny, nz = (2,2,2)
-a = np.linspace(0,2,nx)
+nx, ny, nz = (4,4,4)
+a = np.linspace(-1.5,1.5,nx)
 #print(a)
-b = np.linspace(2,4,ny)
-c = np.linspace(4,6,nz)
+b = np.linspace(-1.5,1.5,ny)
+c = np.linspace(-1.5,1.5,nz)
 #print(x,y)
 xv, yv, zv = np.meshgrid(a,b,c, indexing='xy')
 # print(xv, end="--------")
 
-
+#test
 # input()
 # for l in range(nx):
 #     for m in range(ny):
 #         for n in range(nz):
 #             print(xv)
         
-i=0
-print(xv[i,i,i])
+
+#grid spacing
+h = a[2]-a[1]
+Vext = np.sqrt(1/(xv**2 + yv**2 + zv**2))
+Vext = -Vext
+L = (-2/h**2)*(np.matlib.eye(nx, k=0, dtype=float)) + np.matlib.eye(nx, k=-1, dtype=float) + np.matlib.eye(nx, k=1, dtype=float)
+print(L)
